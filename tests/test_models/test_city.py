3124aa3e7ddcd04ab52c6ca94fb6743b965fedb8
pshemssa <122387932+pshemssa@users.noreply.github.com>
2023-10-08 19:28:51 +0200

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+from models.state import State
+from tests.test_models.test_base_model import TestBaseModel
 from models.city import City
-import os
 
 
-class test_City(test_basemodel):
-    """ tests for city """
+class TestCity(TestBaseModel):
+    """ Test for city"""
 
     def __init__(self, *args, **kwargs):
-        """ init the test class"""
+        """ """
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ testing state_id type """
+        """ """
+        state = State()
         new = self.value()
-        self.assertEqual(type(new.state_id), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        new.state_id = state.id
+        self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
-        """ testing name type"""
+        """ """
         new = self.value()
-        self.assertEqual(type(new.name), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        new.name = "Batch"
+        self.assertEqual(type(new.name), str)
