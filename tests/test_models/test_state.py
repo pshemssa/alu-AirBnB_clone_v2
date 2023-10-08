64dc7a1a969864c1cab105b95e4b2abfab7c73bd
pshemssa <122387932+pshemssa@users.noreply.github.com>
2023-10-08 19:31:05 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 """ """
-from tests.test_models.test_base_model import test_basemodel
+from tests.test_models.test_base_model import TestBaseModel
 from models.state import State
-import os
 
 
-class test_state(test_basemodel):
-    """ states test class"""
+class TestState(TestBaseModel):
+    """Test for state """
 
     def __init__(self, *args, **kwargs):
-        """ state test class init"""
+        """ """
         super().__init__(*args, **kwargs)
-        self.name = "State"
+        self.name = "California"
         self.value = State
 
     def test_name3(self):
-        """ testing state name attr"""
+        """ """
         new = self.value()
-        self.assertEqual(type(new.name), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+        new.name = "Arizona"
+        self.assertEqual(type(new.name), str)
